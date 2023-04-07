2e090216083b9f82ab8364b8f64740fa76b254f9
nealnnn <nealnnn@gmail.com>
2023-04-07 17:40:10 +0000

    Added to adder2Examples

---
+++ x86/adder2Example/addr2posStripped2.s
@@ -0,0 +1,34 @@
+	.file	"adder2.c"
+	.text
+	.globl	adder2
+	.type	adder2, @function
+adder2:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addl	$2, %eax
+	popq	%rbp
+	ret
+.LC0:
+	.string	"x is: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movl	$40, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	adder2
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	ret
